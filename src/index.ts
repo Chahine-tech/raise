b385e4d285173818718b8008c7ebba73c96f6876
Chahine-tech <chahine.benlahcen@gmail.com>
2023-10-21 19:13:45 +0200

    feat: add just add a post request

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,12 @@ export type Env = {
 	DATABASE_URL: string;
 };
 
+type PostBody = {
+	name: string;
+	description: string;
+	price: number;
+};
+
 const app = new Hono<{ Bindings: Env }>();
 
 app.get('/', async (c) => {
@@ -16,6 +22,30 @@ app.get('/', async (c) => {
 		const db = drizzle(client);
 
 		const result = await db.select().from(products);
+
+		return c.json({
+			result,
+		});
+	} catch (error) {
+		console.log(error);
+		return c.json(
+			{
+				error,
+			},
+			400
+		);
+	}
+});
+
+app.post('/', async (c) => {
+	try {
+		const client = new Pool({ connectionString: c.env.DATABASE_URL });
+
+		const db = drizzle(client);
+
+		const body = await c.req.json();
+
+		const result = await db.insert(products).values(body);
 
 		return c.json({
 			result,
